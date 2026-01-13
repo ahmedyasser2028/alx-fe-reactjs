d783b7cecd63521eef54f9e3a97c70a973bccecd
Ahmed.Yasser2028 <ahmed.yasser2028@gmail.com>
2026-01-13 23:39:04 +0200

    Move alx-react-app contents to root

--- alx-react-app/alx-react-app-props/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center', margin: '10px 0' }}>
-      <p style={{ fontSize: '18px', color: '#333' }}>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-
-export default MainContent;
